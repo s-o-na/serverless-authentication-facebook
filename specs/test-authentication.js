2f4e767be5ef5c98069a282a76c4931f4b8c18db
Eetu Tuomala <eetu.tuomala@sc5.io>
2016-03-19 01:00:46 +0200

    fix config in specs, upgrade dependencies

--- specs/test-authentication.js
+++ specs/test-authentication.js
@@ -6,7 +6,7 @@ let auth = require('../lib');
 describe('Facebook authentication', () => {
   describe('Test Facebook authentication', () => {
     it('test facebook ', () => {
-      let providerConfig = config.getConfig('facebook');
+      let providerConfig = config('facebook');
       auth.signin(providerConfig, {}, (err, data) => {
         expect(err).to.be.null;
         expect(data.url).to.equal('https://www.facebook.com/dialog/oauth?client_id=fb-mock-id&redirect_uri=https://api-id.execute-api.eu-west-1.amazonaws.com/dev/callback/facebook&scope=');
