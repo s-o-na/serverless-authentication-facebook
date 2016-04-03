8b7eebdfb16a3300abf164a5a9f6cc24e3110500
Eetu Tuomala <eetu.tuomala@sc5.io>
2016-04-04 01:22:05 +0300

    refactored to extend default provider

--- src/index.js
+++ src/index.js
@@ -1,52 +1,25 @@
 'use strict';
 
-import async from 'async';
-import request from 'request';
-import {utils, Profile} from 'serverless-authentication';
+import {Profile, Provider} from 'serverless-authentication';
 
-export function signin({id, redirect_uri}, {scope, state}, callback) {
-  let params = {
-    client_id: id,
-    redirect_uri
-  };
-  if(scope) {
-    params.scope = scope;
+class FacebookProvider extends Provider {
+  signin({scope, state}, callback){
+    let options = Object.assign(
+      {scope, state},
+      {signin_uri: 'https://www.facebook.com/dialog/oauth'}
+    );
+    super.signin(options, callback);
   }
-  if(state) {
-    params.state = state;
-  }
-  let url = utils.urlBuilder('https://www.facebook.com/dialog/oauth', params);
-  callback(null, {url});
-}
 
-export function callback({code, state}, {id, redirect_uri, secret}, callback) {
-  async.waterfall([
-    (callback) => {
-      let url = utils.urlBuilder('https://graph.facebook.com/v2.3/oauth/access_token', {
-        client_id: id,
-        redirect_uri,
-        client_secret: secret,
-        code
-      });
-      request.get(url, callback);
-    },
-    (response, accessData, callback) => {
-      let {access_token} = JSON.parse(accessData);
-      let url = utils.urlBuilder('https://graph.facebook.com/me', {
-        fields: 'id,name,picture,email',
-        access_token
-      });
-      request.get(url, (error, response, profileData) => {
-        if(!error) {
-          callback(null, mapProfile(JSON.parse(profileData)));
-        } else {
-          callback(err);
-        }
-      });
-    }
-  ], (err, data) => {
-    callback(err, data, state);
-  });
+  callback(event, callback){
+    var options = {
+      authorization_uri: 'https://graph.facebook.com/v2.3/oauth/access_token',
+      profile_uri: 'https://graph.facebook.com/me',
+      profileMap: mapProfile,
+      authorizationMethod: 'GET'
+    };
+    super.callback(event, options, {profile: {fields: 'id,name,picture,email'}}, callback);
+  }
 }
 
 function mapProfile(response) {
@@ -59,3 +32,57 @@ function mapProfile(response) {
     _raw: response
   });
 }
+
+export function signin(config, options, callback) {
+  (new FacebookProvider(config)).signin(options, callback);
+}
+
+export function callback(event, config, callback) {
+  (new FacebookProvider(config)).callback(event, callback);
+}
+
+// export function signin({id, redirect_uri}, {scope, state}, callback) {
+//   let params = {
+//     client_id: id,
+//     redirect_uri
+//   };
+//   if(scope) {
+//     params.scope = scope;
+//   }
+//   if(state) {
+//     params.state = state;
+//   }
+//   let url = utils.urlBuilder('https://www.facebook.com/dialog/oauth', params);
+//   callback(null, {url});
+// }
+//
+// export function callback({code, state}, {id, redirect_uri, secret}, callback) {
+//   async.waterfall([
+//     (callback) => {
+//       let url = utils.urlBuilder('https://graph.facebook.com/v2.3/oauth/access_token', {
+//         client_id: id,
+//         redirect_uri,
+//         client_secret: secret,
+//         code
+//       });
+//       request.get(url, callback);
+//     },
+//     (response, accessData, callback) => {
+//       let {access_token} = JSON.parse(accessData);
+//       let url = utils.urlBuilder('https://graph.facebook.com/me', {
+//         fields: 'id,name,picture,email',
+//         access_token
+//       });
+//       request.get(url, (error, response, profileData) => {
+//         if(!error) {
+//           callback(null, mapProfile(JSON.parse(profileData)));
+//         } else {
+//           callback(err);
+//         }
+//       });
+//     }
+//   ], (err, data) => {
+//     callback(err, data, state);
+//   });
+// }
+
